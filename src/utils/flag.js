494fe14d7eb683624c62bb7fc21557193bd8fc22
ikurtz <ikurtz@cloudbees.com>
2021-03-22 10:36:28 -0500

    made comments on lines 4, 11, and 18

--- src/utils/flag.js
+++ src/utils/flag.js
@@ -1,19 +1,21 @@
 import Rox from 'rox-browser'
 import { betaAccess, isLoggedIn, getCompany } from './users'
 
+// Creating container class & defining our feature flags. Registration happens below on line 37
 export const Flags = {
   score: new Rox.Flag(false),
   ask: new Rox.Flag(false),
   show: new Rox.Flag(false),
   headerColor: new Rox.RoxString('is-dark', ['is-dark', 'is-primary', 'is-white'])
 }
-
+// Defining the configurationFetchedHandler for analytics
 export const configurationFetchedHandler = fetcherResults => {
   if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
     window.location.reload(false)
   }
 }
 
+// Defining the impressionHandler for reporting & analytics
 export const impressionHandler = (reporting, experiment) => {
   if (experiment) {
     console.log('flag ' + reporting.name + ' value is ' + reporting.value + ', it is part of ' + experiment.name + ' experiment')
